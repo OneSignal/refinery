f407e2ffda042ccc894bff10f030d59ae4778384
Joe Wilm <joe@jwilm.com>
2022-01-25 13:51:59 -0800

    Add onesignal publish script

---
+++ build-onesignal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o nounset
+# set -o pipefail
+set -o xtrace
+
+VERSION="1.8.1"
+TAGS="${VERSION}"
+
+unset GOOS
+unset GOARCH
+export KO_DOCKER_REPO=${KO_DOCKER_REPO:-osig}
+export GOFLAGS="-ldflags=-X=main.BuildID=$VERSION"
+export SOURCE_DATE_EPOCH=$(date +%s)
+# shellcheck disable=SC2086
+ko publish \
+  --tags "${TAGS}" \
+  --base-import-paths \
+  --platform "linux/amd64,linux/arm64" \
+  ./cmd/refinery
